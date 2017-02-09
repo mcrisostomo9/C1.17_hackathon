735b944777c70804e4f436465b0396fc158d2434
Mark Crisostomo <mcrisostomo9@gmail.com>
2017-02-08 16:57:14 -0800

    updated dom elements

--- index.js
+++ index.js
@@ -6,7 +6,11 @@
  * document load
  */
 //TODO  click handlers
+$(document).ready(initialize);
 
+function initialize(){
+    $('.search_button').click(bar_search);
+}
 
 // Variables
 var bars_listed = [];
@@ -20,8 +24,28 @@ var bars_added = [];
 
 function bar_search() {
     console.log('search has been clicked');
+    //TODO go to page 2
+    //TODO ajax call to fill bars_listed
+    
+}
+//create DOM elements for page 2
+function bars_to_dom() {
+    var main_container = $('<div>').addClass('col-md5 pull-right');
+    var bar_container = $('<div>').addClass('media');
+
+    var bar_image_container = $('<div>').addClass('media-left media-middle');
+    var bar_image = $('<img>').addClass('media-object');
+
+    var bar_info_container = $('<div>').addClass('media-body');
+    var bar_name = $('<h4>').addClass('media-heading');
+    var bar_info_list = $('<div>').addClass('col-md-8 pull-left');
+    var address = $('<h5>').text('Address: ');//TODO need span with in hv?
+    var hours = $('<h5>').text('Hours: ');//TODO need span with in hv?
+    var phone = $('<h5>').text('Phone: ');//TODO need span with in hv?
+    var reviews = $('<h5>').text('Reviews: ');//TODO need span with in hv?
 
 }
+
 
 /**
  * function will loop through a list and will show up on the (left or right) of the screen with all nearby bars.
